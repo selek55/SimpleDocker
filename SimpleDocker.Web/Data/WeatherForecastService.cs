7dcec36cbf3ab97b6791f6b85eca6b06cdce1be3
Yasin SELEK <selek_55@hotmail.com>
2019-12-13 21:27:21 +0300

    Add project files.

---
+++ SimpleDocker.Web/Data/WeatherForecastService.cs
@@ -0,0 +1,41 @@
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDocker.Web.Data {
+
+    public class JsonContent : StringContent {
+        public JsonContent(object obj) :
+            base(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json") { }
+    }
+
+    public class MyClient : HttpClient {
+        public MyClient() {
+            BaseAddress = new Uri("https://localhost:44338/");
+        }
+
+        public async Task<TOut> PostJsonAsync<TOut, TIn>(string url, TIn model) {
+            var response = await PostAsync(url, new JsonContent(model));
+            var returnValue = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<TOut>(returnValue);
+        }
+
+        public async Task<TOut> GetJsonAsync<TOut>(string url) {
+            var response = await GetAsync(url);
+            var returnValue = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<TOut>(returnValue);
+        }
+    }
+
+    public class WeatherForecastService {
+
+        public Task<WeatherForecast[]> GetForecastAsync() {
+            var httpClient = new MyClient();
+            return httpClient.GetJsonAsync<WeatherForecast[]>("WeatherForecast");
+        }
+    }
+
+
+}
